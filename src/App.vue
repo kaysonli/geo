54c1888c2ec470f9b1ea1f447a47a4944a3a0913
Kayson <lzkwin@qq.com>
2016-05-14 21:38:49 +0800

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <group>
+      <cell title="vue" value="cool">
+      </cell>
+    </group>
+      	<date-time title="日期"></date-time>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+import Group from 'vux/components/group'
+import Cell from 'vux/components/cell'
+import DateTime from 'vux/components/datetime'
+// require('vux/vux.css');
+
+export default {
+  components: {
+    Hello, Group, Cell, DateTime
+  }
+}
+</script>
+
+<style>
+@import url("./../node_modules/vux/vux.css");
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
