2052bb55626c2a8144480fd0432a6086021b1d6c
Kayson <lzkwin@qq.com>
2016-05-16 22:47:00 +0800

    Update login page.

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="login">
+        <img class="logo" src="./resources/images/logo.png">
         <div class="text-field flex-box">
             <i class="fa fa-user"></i>
-            <input type="text">
-            <i class="fa fa-remove"></i>
+            <input type="text" v-el:userName>
+            <i class="fa fa-remove" @click="clear"></i>
         </div>
         <div class="text-field flex-box">
             <i class="fa fa-lock"></i>
-            <input type="text">
+            <input type="password">
             <span class="forgot">忘记密码</span>
         </div>
         <div class="btn">登录</div>
@@ -18,27 +19,43 @@
 <script>
 export default {
     methods: {
-        viewList() {
-            this.$router.go('/list');
-        },
-        addDevice() {
-            this.$router.go('/add');
+        clear() {
+            this.$els.username.value = '';
+            this.$els.username.focus();
         }
     }
 }
 </script>
 <style scoped>
+    .login {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
+    .logo {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
     .text-field {
         margin: 16px;
         border: 1px solid #999;
         border-radius: 20px;
         background: #fff;
         align-items: center;
+        width: 80%;
+        height: 50px;
+        /*box-sizing: border-box;*/
     }
     .text-field input {
         flex: 1;
         border: none;
         outline: 0;
+        font-size: 16px;
     }
     .text-field input:focus {
         border: none;
@@ -51,12 +68,16 @@ export default {
         width: 30px;
     }
     .forgot {
-        color: #3BE63B;
+        color: yellowgreen;
         margin-right: 10px;
+        white-space: nowrap;
     }
     .btn {
-        background: #3BE63B;
+        background: yellowgreen;
         color: #fff;
+        width: 80%;
+        margin: 16px;
+        height: 47px;
     }
     .register {
         color: brown;
