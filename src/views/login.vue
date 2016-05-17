74bb663ca09a87e54c619461d51373249efd9a48
Kayson Li <kayson.li@ruifusoft.com>
2016-05-17 19:33:17 +0800

    Update login page.

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,18 +1,15 @@
 <template>
     <div class="login">
-        <img class="logo" src="./resources/images/logo.png">
-        <div class="text-field flex-box">
-            <i class="fa fa-user"></i>
-            <input type="text" v-el:userName>
-            <i class="fa fa-remove" @click="clear"></i>
+        <img class="logo" src="./resources/images/login.png">
+        <div class="text-field">
+            <input type="text" placeholder="请输入手机号" v-el:userName>
         </div>
-        <div class="text-field flex-box">
-            <i class="fa fa-lock"></i>
-            <input type="password">
-            <span class="forgot">忘记密码</span>
+        <div class="text-field">
+            <input type="password" placeholder="请输入密码" v-el:password>
         </div>
-        <div class="btn">登录</div>
-        <div class="register center" @click="register">没有账户，点击注册</div>
+        <div class="btn" @click="login">登录</div>
+        <div class="btn" @click="register">注册</div>
+        <div class="forgot center">忘记密码</div>
     </div>
 </template>
 
@@ -23,6 +20,9 @@ export default {
             this.$els.username.value = '';
             this.$els.username.focus();
         },
+        login() {
+
+        },
         register() {
             this.$router.go('./register');
         }
@@ -32,6 +32,7 @@ export default {
 <style scoped>
     .login {
         display: flex;
+        background: #fff;
         flex-direction: column;
         align-items: center;
         position: absolute;
@@ -41,21 +42,15 @@ export default {
         bottom: 0;
     }
     .logo {
-        margin-top: 50px;
-        margin-bottom: 50px;
+        height: 50%;
     }
     .text-field {
-        margin: 16px;
+        width: 80%;
         padding-left: 0;
-        border: 1px solid #999;
-        border-radius: 20px;
-        background: #fff;
-        align-items: center;
-        height: 50px;
-        /*box-sizing: border-box;*/
+        border-bottom: 1px solid #ECECEC;
     }
     .text-field input {
-        flex: 1;
+        width: 100%;
         border: none;
         outline: 0;
         font-size: 16px;
@@ -64,22 +59,15 @@ export default {
         border: none;
         -webkit-appearance: none;
     }
-    .fa {
-        font-size: 20px;
-    }
-    .fa-remove {
-        width: 30px;
-    }
     .forgot {
-        color: yellowgreen;
-        margin-right: 10px;
-        white-space: nowrap;
+        color: #A4A4A4;
+        margin-top: 16px;
     }
     .btn {
-        background: yellowgreen;
-        color: #fff;
+        background: #FEDA00;
         width: 80%;
-        margin: 16px;
+        margin-top: 10px;
+        margin-bottom: 0;
         height: 47px;
     }
     .register {
